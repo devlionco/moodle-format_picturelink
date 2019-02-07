4df3816fb70cb5f6a03a7103aad09f9475296985
Ilya Dymshits <ilya@devlion.co>
2019-02-07 13:38:57 +0200

    TD#216 (update) design for description popup

--- amd/src/tooltipmodal.js
+++ amd/src/tooltipmodal.js
@@ -25,8 +25,7 @@ define([ 'jquery', 'core/str' ], function($, str) {
 
 	return {
 		init: function() {
-			$('.p_locked, .p_hide').on('click', handlerIn);
-			// $('.p_locked, .p_hide').hover(handlerIn, handlerOut);
+			$('.p_locked, .p_hide').hover(handlerIn, handlerOut);
 		}
 	};
 
